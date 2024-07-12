8a3548a786251aa2bfaac8b8a648c1b1333d70e3
Pifou25 <nicolas_gras@hotmail.com>
2024-07-12 23:19:01 +0200

    feat: automatic restore any gz file in backup
    fix the trap stop function

--- install/OS_specific/Docker/init.sh
+++ install/OS_specific/Docker/init.sh
@@ -21,117 +21,101 @@ service_mariadb(){
   return 0
 }
 
-docker_stop() {
-  echo "${JAUNE}Stopping Jeedom container${NORMAL}"
-  echo "${VERT}Stopping Apache gracefully${NORMAL}"
-  service apache2 stop
-  if [[ 1 -eq ${ISMARIABDBINSTALLED} ]]; then
-     echo "${VERT}Stopping Database gracefully${NORMAL}"
-     service mariadb stop
-  fi
-  echo "${VERT}Stopping ATD gracefully${NORMAL}"
-  service atd stop
-  echo "${ROUGE}Requesting stop on init.sh${NORMAL}"
-  echo "${VERT}Killing CRON${NORMAL}"
-  killall cron
-  exit 0
+docker_stop(){
+	echo "${JAUNE}Signal $@ Stopping Jeedom container${NORMAL}"
+	echo "${VERT}Killing CRON${NORMAL}"
+	killall cron
+	echo "${VERT}Stopping Apache gracefully${NORMAL}"
+	service apache2 stop
+	echo "${VERT}Stopping Database gracefully${NORMAL}"
+	service mariadb stop
+	echo "${VERT}Stopping ATD gracefully${NORMAL}"
+	service atd stop
+	echo "${VERT}Stopping fail2ban gracefully${NORMAL}"
+	service fail2ban stop
+	echo "${ROUGE}Requesting stop on init.sh${NORMAL}"
+	touch ${FILE_STOP}
+	exit 0
 }
 
-setTimeZone() {
-  [[ ${TZ} == $(</etc/timezone) ]] && return
-  echo "Setting timezone to ${TZ}"
-  ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime
-  dpkg-reconfigure -fnoninteractive tzdata
+say() {
+  if [[ -z "$2" ]]; then COLOR=$VERT; else COLOR=$2; fi
+  echo "******************************
+${COLOR}$1${NORMAL}
+******************************"
 }
 
-set_root_password(){
-  #set root password
-  if [[ -z ${ROOT_PASSWD} ]]; then
-    ROOT_PASSWD=$(openssl rand -base64 32 | tr -d /=+ | cut -c1-15)
-    echo "Use generate password : ${ROOT_PASSWD}"
-  fi
-  echo "root:${ROOT_PASSWD}" | chpasswd
-}
-
-apache_setup() {
-  mkdir -p /var/log/apache2/
-  #define ports, activate ssl
-  if [[ 3 -ne $(grep -cP "(${APACHE_HTTP_PORT}|${APACHE_HTTPS_PORT})" /etc/apache2/ports.conf) ]]; then
-    echo "Ports update for apache2: ${APACHE_HTTP_PORT}, ${APACHE_HTTPS_PORT}"
-    echo "Listen ${APACHE_HTTP_PORT}
-	<IfModule ssl_module>
-		Listen ${APACHE_HTTPS_PORT:-443}
-	</IfModule>
-
-	<IfModule mod_gnutls.c>
-		Listen ${APACHE_HTTPS_PORT:-443}
-	</IfModule>" >/etc/apache2/ports.conf
-    sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:${APACHE_HTTP_PORT}/" /etc/apache2/sites-available/000-default.conf
-    sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:${APACHE_HTTPS_PORT}/" /etc/apache2/sites-available/default-ssl.conf
-  fi
-}
-
-echo 'Start init'
+# flag to fail fast on errors
+set -e
 
 # $WEBSERVER_HOME and $VERSION env variables comes from Dockerfile
 
-if [ -f ${WEBSERVER_HOME}/core/config/common.config.php ]; then
-	echo 'Jeedom is already install'
-	JEEDOM_INSTALL=1
-else
-	echo 'Start jeedom installation'
-	JEEDOM_INSTALL=0
-	rm -rf /root/install.sh
-	wget https://raw.githubusercontent.com/jeedom/core/${VERSION}/install/install.sh -O /root/install.sh
-	chmod +x /root/install.sh
-	/root/install.sh -s 6 -v ${VERSION} -w ${WEBSERVER_HOME}
-	if [ $(which mysqld | wc -l) -ne 0 ]; then
-		chown -R mysql:mysql /var/lib/mysql
-		mysql_install_db --user=mysql --basedir=/usr/ --ldata=/var/lib/mysql/
-		service_mariadb restart
-		MYSQL_JEEDOM_PASSWD=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 15)
-		echo "DROP USER 'jeedom'@'localhost';" | mysql > /dev/null 2>&1
-		echo  "CREATE USER 'jeedom'@'localhost' IDENTIFIED BY '${MYSQL_JEEDOM_PASSWD}';" | mysql
+say "Start mariadb and atd services"
+
+service mariadb start
+service mariadb status
+
+service atd restart
+service atd status
+
+if [[ -f ${WEBSERVER_HOME}/initialisation ]]; then
+
+	if [ ! -f "/var/www/html/core/config/common.config.php" ]; then
+		#
+		# nouvelle installation
+		#
+	    say "Start JEEDOM initialisation !"
+		JEEDOM_INSTALL=1
+
+		DB_PASSWORD=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 15)
+		echo "DROP USER IF EXISTS 'jeedom'@'localhost';" | mysql
+		echo  "CREATE USER 'jeedom'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';" | mysql
 		echo  "DROP DATABASE IF EXISTS jeedom;" | mysql
 		echo  "CREATE DATABASE jeedom;" | mysql
 		echo  "GRANT ALL PRIVILEGES ON jeedom.* TO 'jeedom'@'localhost';" | mysql
+
 		cp ${WEBSERVER_HOME}/core/config/common.config.sample.php ${WEBSERVER_HOME}/core/config/common.config.php
-		sed -i "s/#PASSWORD#/${MYSQL_JEEDOM_PASSWD}/g" ${WEBSERVER_HOME}/core/config/common.config.php
-		sed -i "s/#DBNAME#/jeedom/g" ${WEBSERVER_HOME}/core/config/common.config.php
-		sed -i "s/#USERNAME#/jeedom/g" ${WEBSERVER_HOME}/core/config/common.config.php
-		sed -i "s/#PORT#/3306/g" ${WEBSERVER_HOME}/core/config/common.config.php
-		sed -i "s/#HOST#/localhost/g" ${WEBSERVER_HOME}/core/config/common.config.php
-		/root/install.sh -s 10 -v ${VERSION} -w ${WEBSERVER_HOME}
-		/root/install.sh -s 11 -v ${VERSION} -w ${WEBSERVER_HOME}
+		sed -i "s/#PASSWORD#/${DB_PASSWORD}/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#DBNAME#/${DB_NAME:-jeedom}/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#USERNAME#/${DB_USERNAME:-jeedom}/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#PORT#/${DB_PORT:-3306}/g" ${WEBSERVER_HOME}/core/config/common.config.php
+		sed -i "s/#HOST#/${DB_HOST:-localhost}/g" ${WEBSERVER_HOME}/core/config/common.config.php
+
+		if [ -d "${WEBSERVER_HOME}/backup" ] && [ "$(ls -At ${WEBSERVER_HOME}/backup/*.gz)" ]; then
+		  RESTOREFILE=$(ls -At ${WEBSERVER_HOME}/backup/*.gz |  head -n1)
+		  say "found a backup ${RESTOREFILE}, restore ongoing"
+		  php ${WEBSERVER_HOME}/install/restore.php backup=${RESTOREFILE}
+		else
+			say "start JEEDOM PHP script installation"
+			php ${WEBSERVER_HOME}/install/install.php mode=force
+		fi
+
+	else
+		#
+		# nouveau container, volumes partagés
+		# Jeedom déjà configuré
+		#
+		say "NEW Jeedom container for existing installation"
 	fi
+
+	# remove default fail2ban, contains useless sshd check
+	rm -f /etc/fail2ban/jail.d/defaults-debian.conf
+
+	# remove the flag file after the first successfull installation
+	rm ${WEBSERVER_HOME}/initialisation
+else
+    say "Resuming container..." $JAUNE
 fi
 
-setTimeZone
-#setup apache port
-apache_setup
-#setup root passwd
-set_root_password
-
-echo 'Start atd'
-service atd restart
-
-if [ $(which mysqld | wc -l) -ne 0 ]; then
-	echo 'Starting mariadb'
-	chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
-	service_mariadb restart
-	if [ $? -ne 0 ]; then
-		 rm /var/lib/mysql/ib_logfile*
-		 service_mariadb restart
+if [[ ${JEEDOM_INSTALL} == 1 ]]; then
+	if [[ ! -z "${RESTOREBACKUP}" ]] && [[ "${RESTOREBACKUP}" != 'NO' ]]; then
+		say "Need restore backup ${RESTOREBACKUP}"
+		wget ${RESTOREBACKUP} -O /tmp/backup.tar.gz
+		php ${WEBSERVER_HOME}/install/restore.php backup=/tmp/backup.tar.gz
+		rm /tmp/backup.tar.gz
 	fi
-fi
-
-if [ ${JEEDOM_INSTALL} -eq 0 ] && [ ! -z "${RESTOREBACKUP}" ] && [ "${RESTOREBACKUP}" != 'NO' ]; then
-	echo 'Need restore backup '${RESTOREBACKUP}
-	wget ${RESTOREBACKUP} -O /tmp/backup.tar.gz
-	php ${WEBSERVER_HOME}/install/restore.php backup=/tmp/backup.tar.gz
-	rm /tmp/backup.tar.gz
-	if [ ! -z "${UPDATEJEEDOM}" ] && [ "${UPDATEJEEDOM}" != 'NO' ]; then
-		echo 'Need update jeedom'
+	if [[ ! -z "${UPDATEJEEDOM}" ]] && [[ "${UPDATEJEEDOM}" != 'NO' ]]; then
+		say "Need update jeedom ? '${UPDATEJEEDOM}'"
 		php ${WEBSERVER_HOME}/install/update.php
 	fi
 fi
@@ -145,7 +129,29 @@ chown -R www-data:www-data ${WEBSERVER_HOME}
 echo 'Start apache2'
 service apache2 start
 
-echo "Add trap docker_stop"
+echo 'Start fail2ban'
+service fail2ban start
+service fail2ban status
+
+echo 'Start CRON daemon'
+cron
+
+# step_12_jeedom_check
+sh /tmp/install.sh -s 12 -v ${VERSION} -w ${WEBSERVER_HOME} -i docker
+
+#TAKE CARE : the init.sh script is running under sh so trap only takes signal_number
+echo 'trap signal 15 SIGTERM causes docker_stop'
 trap "docker_stop $$ ;" 15
 
-cron -f
+if [[ ${JEEDOM_INSTALL} == 1 ]]; then
+    say "Sucessfull installation !
+Jeedom is now up and running
+https://doc.jeedom.com/fr_FR/premiers-pas/"
+else
+    say "Jeedom is now up and running"
+fi
+
+# infinite loop until stop requested
+while [[ ! -e "${FILE_STOP}" ]]; do sleep 1; done
+# gracefully stop
+docker_stop "${FILE_STOP}"
